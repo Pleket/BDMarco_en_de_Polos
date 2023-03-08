e8a12737a15eac87496803bd7338c5822286c22a
liekebiggel <liekebiggel@users.noreply.github.com>
2023-03-08 16:45:37 +0100

    Update main.py

--- main.py
+++ main.py
@@ -51,24 +51,6 @@ def q2(spark_context: SparkContext, data_frame: DataFrame):
 
 def q3(spark_context: SparkContext, rdd: RDD):
     # TODO: Imlement Q3 here
-
-    def compute_variance(line: str):
-        len_X = len(line)
-
-        mu_X = 0 
-        quadratic = 0
-        for value in line:
-            value_int = value
-            intermediate = value_int / len_X
-            mu_X += intermediate
-        
-            quad = value_int**2
-            quad = quad / len_X
-            quadratic += quad
-        
-        variance = quadratic - mu_X**2
-
-        return variance
         
     #Get RDD in form [['val', 'nr'], [ , ], [ , ] ]
     rdd_splitted = rdd.map( lambda line: line.split(","))
@@ -102,16 +84,10 @@ def q3(spark_context: SparkContext, rdd: RDD):
 
         return sum_square - mu_X**2
 
-    rdd_key_value = rdd_combine.map(lambda tuple: (tuple[0][0] + tuple[1][0] + tuple[2][0], [tuple[0][1], tuple[1][1], tuple[2][1]]))
-    rdd_split = rdd_key_value.map(lambda line: (line[0], [line[1][0].split(";"), line[1][1].split(";"), line[1][2].split(";")]))
-    rdd_aggregate = rdd_split.map(lambda line: (line[0], aggregated_vecs(line[1])))
-                        #   .map(lambda line: (line[0], compute_variance(line[1])))
-
-    rdd_vars = rdd_aggregate.map(lambda line: (line[0], rdd_variance(line[1])))
-    
-    # rdd_variance = rdd_aggregate.map(lambda line: (line[0], line[1].map(lambda x: x**2 / len(line[1])), line[1].map(lambda x: x / len(line[1]))))\
-    #                 .map(lambda line: (line[0], line[1].reduce(lambda x, y: x + y), line[1].reduce(lambda x, y: x + y)))\
-    #                 .map(lambda line: (line[0], line[1] - line[2]**2))
+    rdd_vars = rdd_combine.map(lambda tuple: (tuple[0][0] + tuple[1][0] + tuple[2][0], [tuple[0][1], tuple[1][1], tuple[2][1]]))\
+                    .map(lambda line: (line[0], [line[1][0].split(";"), line[1][1].split(";"), line[1][2].split(";")]))\
+                    .map(lambda line: (line[0], aggregated_vecs(line[1])))\
+                    .map(lambda line: (line[0], rdd_variance(line[1])))
     
     rdd_under_410 = rdd_vars.filter(lambda line: line[1] <= 410)
     #rdd_under_20 = rdd_under_410.filter(lambda line: line[1] <= 20)
