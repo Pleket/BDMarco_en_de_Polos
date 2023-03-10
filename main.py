1a410ea2505f43f7f90e285ba33776897b127567
liekebiggel <liekebiggel@users.noreply.github.com>
2023-03-10 16:03:04 +0100

    Update main.py

--- main.py
+++ main.py
@@ -99,49 +99,61 @@ def q2(spark_context: SparkContext, data_frame: DataFrame):
 
 
 def q3(spark_context: SparkContext, rdd: RDD):
-    TAU_PARAMETER = 410
+
+    def agg_var(agg_vec):
+        som = 0
+        som_squared = 0
+
+        for value in agg_vec:
+            som += value
+            som_squared += value*value
+
+        length_vector = len(agg_vec)
+        avg = 1 / length_vector * som
+        avg_sq = avg*avg
+        sq_som = 1 / length_vector * som_squared
+        
+        return sq_som - avg_sq
+
+    def agg_vec1(line, vs, vi):
+        
+        mapping = [( (line[0], line2[0]), tuple(nr[0] + nr[1] for nr in zip(line[1], line2[1]))) 
+                    for line2 in vs.value[ vi.value [line[0]] + 1:] 
+                    if vi.value[ line[0]] != len(vs.value)-1]
+
+        return mapping
+    
+    def agg_vec2(line, vs, vi):
+        
+        mapping = [( line[0][0] + line[0][1] + line3[0], 
+                    tuple(nr[0] + nr[1] for nr in zip(line[1], line3[1]))) 
+                    for line3 in vs.value[ vi.value[ line[0][1] ] + 1 : ] 
+                    if vi.value[ line[0][1] ] != len(vs.value)-1]
+
+        return mapping
+
+    #Get the vectors from the RDD and sort the values in the vector
     vecs = rdd.collect()
     vecs.sort(key=lambda item: item[0])
+
+    #Take the index only
     vec_index = {vecs[i][0]: i for i in range(len(vecs))}
 
-    inv_vec_dim = 1/ vec_dims
-    # Version map
-    def get_var(agg_vec):
-        total_sum = 0
-        total_sum_squared = 0
-        for val in agg_vec:
-            total_sum+=val
-            total_sum_squared+=val*val
-        return 1/vec_dims * (total_sum_squared - (1/vec_dims * total_sum * total_sum))
-
-
-    # #BROADCASSTING
+    #Use broadcasting to store the names of the vectors
     vi = spark_context.broadcast(vec_index)
     vs = spark_context.broadcast(vecs)
-    rdd = rdd.repartition(8).flatMap(lambda x: 
-                    [( (x[0],mapping[0]), tuple(y[0]+y[1] for y in zip(x[1], mapping[1]))) for mapping in vs.value[vi.value[x[0]]+1: ] if vi.value[x[0]] != len(vs.value)-1]
-                    ) \
-            .flatMap(lambda x: 
-                    [( x[0][0] + x[0][1] + mapping[0], tuple(y[0]+y[1] for y in zip(x[1], mapping[1]))) for mapping in vs.value[vi.value[x[0][1]]+1: ] if vi.value[x[0][1]] != len(vs.value)-1]
-                    )\
-            .map(lambda row: (row[0], get_var(row[1]))) \
-            .filter(lambda row: row[1] < TAU_PARAMETER) \
-                
-    # vi = spark_context.broadcast(vec_index)
-    # vs = spark_context.broadcast(vecs)
-    # rdd = rdd.flatMap(lambda x: 
-    #                 [( (x[0],mapping[0]), tuple(y[0]+y[1] for y in zip(x[1], mapping[1]))) for mapping in vs.value[vi.value[x[0]]+1: ] if vi.value[x[0]] != len(vs.value)-1]
-    #                 ) \
-    #         .flatMap(lambda x: 
-    #                 [( x[0][0] + x[0][1] + mapping[0], tuple(y[0]+y[1] for y in zip(x[1], mapping[1]))) for mapping in vs.value[vi.value[x[0][1]]+1: ] if vi.value[x[0][1]] != len(vs.value)-1]
-    #                 ) \
-    #         .flatMap(lambda x: [(x[0] + str(i), (x[1][i], x[1][i]**2)) for i in range(len(x[1]))]) \
-    #         .reduceByKey(lambda a,b: (a[0] + b[0], a[1]+b[1])) \
-    #         .map(lambda x: (x[0], 1/vec_dims * (x[1][1] - 1/vec_dims * x[1][0]*x[1][0]) )  ) \
-    #         .filter(lambda row: row[1] < TAU_PARAMETER) 
+
+    #Split the data into partitions to distribute the work load
+    rdd = rdd.repartition(8).flatMap(lambda line: agg_vec1(line, vs, vi))\
+                            .flatMap(lambda line: agg_vec2(line, vs, vi))\
+                            .map(lambda line: (line[0], agg_var(line[1])))
+    
+    rdd_410 = rdd.filter(lambda line: line[1] < 410)
+    rdd_20 = rdd_410.filter(lambda line: line[1] < 20)
           
     #print(f">> {rdd.collect()}")
-    print(f">>COUNT {rdd.count()}")
+    print(f">>COUNT {rdd_410.count()}")
+    print(f">>COUNT {rdd_20.count()}")
     return
     
 def q4(spark_context: SparkContext, rdd: RDD):
