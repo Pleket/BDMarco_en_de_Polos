d579da6154b8c47fa4adddcb751baf3f8e8cc6fc
Pleket <50240262+Pleket@users.noreply.github.com>
2023-03-21 15:30:09 +0100

    Generalizations

--- Q4.py
+++ Q4.py
@@ -15,6 +15,7 @@ from collections import defaultdict
 from pyspark.mllib.random import RandomRDDs
 import numpy as np
 import hashlib
+import random as rd
 
 class Q4:
     def __init__(self, epsilon=0.001, delta=0.1):
@@ -31,7 +32,10 @@ class Q4:
         # depth = int(np.ceil(np.log(1 / self.delta)))
 
         # define hash functions
-        hash = [hashlib.sha1, hashlib.md5, hashlib.sha224, hashlib.sha256]
+        hash = []
+        for i in range(self.depth):
+            prime = rd.choice([x for x in range(self.width, self.width * 2) if not [t for t in range(2, x) if not x % t]])
+            hash.append(Hash_Function(prime, self.width))
 
         # initialize CMS array
         cms = defaultdict(lambda: np.zeros(self.width))
@@ -40,11 +44,19 @@ class Q4:
         def cms_update(vec):
             for i in range(len(vec)):
                 for j in range(self.depth):
-                    h = hash[j](str(i).encode() + str(vec[i]).encode()).hexdigest()
-                    idx = int(h, 16) % self.width
+                    idx = hash[j].get_index(str(i).encode() + str(vec[i]).encode())
                     cms[j][idx] += 1
             return cms
 
         # compute CMS sketch of each vector
         cms_vectors = rdd.map(lambda x: (x[0], cms_update(x[1])))
         cms_vectors.foreach(print)
+
+class Hash_Function:
+    def __init__(self, mod_prime, mod_width):
+        self.mod_prime = mod_prime
+        self.mod_width = mod_width
+        self.multiple = rd.randint(1, 30)
+    
+    def get_index(self, key):
+        return ((key * self.multiple) % self.mod_prime) % self.mod_width
